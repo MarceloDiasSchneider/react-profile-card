1d967c5ec0d7130c93c72d5a5e12b1a8eff9768f
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-04-20 11:08:52 +0200

    profile card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [contact, setContact] = useState({
+    firstName: "Marcelo",
+    lastName: "Schneider",
+    phone: "+39 (349) 796-6478",
+    email: "marcelo.d.schneider@gmail.com",
+    isFavorite: false
+})
+
+  let starIcon = contact.isFavorite ? "star-filled.svg" : "star-empty.svg";
+
+  function toggleFavorite() {
+    setContact((contact) => ({
+      ...contact,
+      isFavorite: !contact.isFavorite,
+    }));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article className="card">
+        <img src="./images/avatar.svg" className="card--image" alt="avatar" />
+        <div className="card--info">
+          <img
+            src={`../images/${starIcon}`}
+            className="card--favorite"
+            onClick={toggleFavorite}
+            alt="star"
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
   );
 }
-
-export default App;
